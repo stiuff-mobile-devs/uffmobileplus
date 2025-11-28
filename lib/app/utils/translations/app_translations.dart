903aff18d7bb5bb4aef24fae79244bcec05d9357
João Vítor L. Gonçalves <jvlgoncalves@id.uff.br>
2025-11-28 13:34:07 -0300

    refactor: agora há um arquivo separado para cada idioma

--- lib/app/utils/translations/app_translations.dart
+++ lib/app/utils/translations/app_translations.dart
@@ -1,5 +1,6 @@
 import 'package:get/get_navigation/src/root/internacionalization.dart';
 import 'package:uffmobileplus/app/utils/translations/en_US/en_us_translation.dart';
+import 'package:uffmobileplus/app/utils/translations/it_IT/it_it_translation.dart';
 import 'package:uffmobileplus/app/utils/translations/pt_BR/pt_br_translation.dart';
 
 class AppTranslation extends Translations {
@@ -7,5 +8,6 @@ class AppTranslation extends Translations {
   Map<String, Map<String, String>> get keys => {
     'pt_BR' : ptBR,
     'en_US' : enUS,
+    'it_IT' : itIT,
   };
 }
