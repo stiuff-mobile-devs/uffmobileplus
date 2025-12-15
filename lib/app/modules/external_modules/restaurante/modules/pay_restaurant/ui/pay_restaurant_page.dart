9c5fb14ddd5af01ce552de3499653483e0290e08
JpFontinele-UFF <jfontinele@id.uff.br>
2025-12-15 10:18:13 -0300

    feat: avisando do saldo

--- lib/app/modules/external_modules/restaurante/modules/pay_restaurant/ui/pay_restaurant_page.dart
+++ lib/app/modules/external_modules/restaurante/modules/pay_restaurant/ui/pay_restaurant_page.dart
@@ -40,102 +40,136 @@ class PayRestaurantPage extends GetView<PayRestaurantController> {
                 decoration: BoxDecoration(
                   gradient: AppColors.darkBlueToBlackGradient(),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      child: Container(
-                        margin: EdgeInsets.only(bottom: 20),
-                        child: IdCard(
-                          userImageUrl: controller.userImageUrl,
-                          username: controller.userName,
-                          iduff: controller.userIdUFF,
-                        ),
+                child: Builder(
+                  builder: (context) {
+                    final balance = double.parse(
+                      controller.currentBalance.value.toString().replaceAll(
+                        ',',
+                        '.',
                       ),
-                    ),
-                    Row(
+                    );
+                    return Column(
                       mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        Text(
-                          "Saldo Atual: ",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.blue[100],
-                          ),
-                        ),
-                        Text(
-                          "R\$ ${controller.currentBalance.value}",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.blue[100],
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 260,
-                      child: Container(
-                        margin: EdgeInsets.only(top: 20, bottom: 24),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            text:
-                                'O valor não será debitado caso o código não seja utilizado.',
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: Colors.blue[200],
-                              fontStyle: FontStyle.italic,
+                        SizedBox(
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 20),
+                            child: IdCard(
+                              userImageUrl: controller.userImageUrl,
+                              username: controller.userName,
+                              iduff: controller.userIdUFF,
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xff052750),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          vertical: 18,
-                          horizontal: 18,
-                        ),
-                      ),
-                      onPressed: () {
-                        controller.goToPaymentTicket();
-                      },
-                      child: Text(
-                        "Gerar Código",
-                        style: TextStyle(fontSize: 20.0, color: Colors.white),
-                      ),
-                    ),
-                    Container(
-                      width: 220,
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(top: 20),
-                      child: TextButton(
-                        onPressed: () {
-                          controller.goToPaymentHelpScreen();
-                        },
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.help, color: Colors.white, size: 24),
-                            Container(
-                              margin: EdgeInsets.only(right: 14),
-                              child: Text(
-                                " Como utilizar o código",
-                                style: TextStyle(color: Colors.white),
+                          children: <Widget>[
+                            Text(
+                              "Saldo Atual: ",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.blue[100],
+                              ),
+                            ),
+                            Text(
+                              "R\$ ${controller.currentBalance.value}",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: balance < 0.70
+                                    ? Colors.red
+                                    : Colors.blue[100],
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ],
                         ),
-                      ),
-                    ),
-                  ],
+                        SizedBox(height: 12),
+                        if (balance < 0.70)
+                          Text(
+                            "Por favor, recarregue para poder usar o bandejão",
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.red[300],
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        else
+                          Text(
+                            "Refeições disponíveis: ${(balance / 0.70).floor()}",
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.green[300],
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        SizedBox(
+                          width: 260,
+                          child: Container(
+                            margin: EdgeInsets.only(top: 20, bottom: 24),
+                            child: RichText(
+                              textAlign: TextAlign.center,
+                              text: TextSpan(
+                                text:
+                                    'O valor não será debitado caso o código não seja utilizado.',
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.blue[200],
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xff052750),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 18,
+                              horizontal: 18,
+                            ),
+                          ),
+                          onPressed: () {
+                            controller.goToPaymentTicket();
+                          },
+                          child: Text(
+                            "Gerar Código",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 220,
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.only(top: 20),
+                          child: TextButton(
+                            onPressed: () {
+                              controller.goToPaymentHelpScreen();
+                            },
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.help, color: Colors.white, size: 24),
+                                Container(
+                                  margin: EdgeInsets.only(right: 14),
+                                  child: Text(
+                                    " Como utilizar o código",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
                 ),
               ),
       ),
