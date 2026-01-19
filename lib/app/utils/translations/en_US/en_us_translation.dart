20a87ccfee1b847b25fc3d0112d22779623d840b
JpFontinele-UFF <jfontinele@id.uff.br>
2026-01-19 10:23:04 -0300

    Update en_us_translation.dart

--- lib/app/utils/translations/en_US/en_us_translation.dart
+++ lib/app/utils/translations/en_US/en_us_translation.dart
@@ -1,20 +1,21 @@
 final Map<String, String> enUS = {
   // Persistent Tab View
-  'atalhos' : 'Shortcuts',
-  'servicos' : 'Services',
-  'configuracoes' : 'Settings',
+  'atalhos': 'Shortcuts',
+  'servicos': 'Services',
+  'configuracoes': 'Settings',
 
   // Tela atalhos
-  'boas-vindas' : 'Welcome to the new',
+  'boas-vindas': 'Welcome to the new',
   'em_desenvolvimento': 'App under development',
-  'trab_constante': 'We are constantly working to deliver the best possible experience. Look forward to the next updates!',
+  'trab_constante':
+      'We are constantly working to deliver the best possible experience. Look forward to the next updates!',
   'versao': 'Version',
 
   // Login Page
-  'entrar' : 'Sign in',
+  'entrar': 'Sign in',
   'qr_code_carteirinha': 'ID Card QR Code',
   'escolha_método_login': 'Choose login method',
-  'sem_login' : 'No Login',
+  'sem_login': 'No Login',
 
   // Choose Profile Page
   'escolha_perfil': 'Choose your Profile',
@@ -25,37 +26,38 @@ final Map<String, String> enUS = {
   'terceirizado': 'Contractor',
 
   // Tela Configuracoes
-  'sobre' : 'About',
-  'sobre_descricao' : 'Application details',
-  'ling_descricao' : 'Change App language',
-  'sair' : 'Logout',
-  'sair_descricao' : 'Log in as a different user',
+  'sobre': 'About',
+  'sobre_descricao': 'Application details',
+  'ling_descricao': 'Change App language',
+  'sair': 'Logout',
+  'sair_descricao': 'Log in as a different user',
 
   // Developer / errors
-  'modo_desenvolvedor' : 'Developer Mode',
-  'desativar_modo_desenvolvedor' : 'DISABLE DEVELOPER MODE TO OPEN THE APP!',
-  'erro' : 'Error',
-  'plataforma_nao_suportada' : 'Unsupported platform',
-  'nao_foi_possivel_abrir_as_configuracoes' : 'Could not open settings',
+  'modo_desenvolvedor': 'Developer Mode',
+  'desativar_modo_desenvolvedor': 'DISABLE DEVELOPER MODE TO OPEN THE APP!',
+  'erro': 'Error',
+  'plataforma_nao_suportada': 'Unsupported platform',
+  'nao_foi_possivel_abrir_as_configuracoes': 'Could not open settings',
 
   // Carteirinha Digital
-  'carteirinha_digital' : 'Digital ID Card',
-  'documento' : 'Document',
-  'matricula' : 'ID',
-  'validade' : 'Expiration Date',
-  'curso' : 'Course',
-  'validador_instrucao' : 'Validate the generated code by using the application',
+  'carteirinha_digital': 'Digital ID Card',
+  'documento': 'Document',
+  'matricula': 'ID',
+  'validade': 'Expiration Date',
+  'curso': 'Course',
+  'validador_instrucao': 'Validate the generated code by using the application',
 
   // Papers
-  'periodicos' : 'Papers',
-  'escolha' : 'Choose one of the options below',
+  'periodicos': 'Papers',
+  'escolha': 'Choose one of the options below',
 
   // Help
-  'ajuda' : 'Help',
+  'ajuda': 'Help',
 
   // Radio Pop Goiaba
-  'radio_name' : 'Radio Pop Goiaba',
-  'radio_description' : 'Radio Pop Goiaba/UFF is an extension project of Universidade Federal Fluminense linked to NUFEP - Núcleo Fluminense de Estudos e Pesquisas da UFF and coordinated by Professor Roberto Kant de Lima.',
+  'radio_name': 'Radio Pop Goiaba',
+  'radio_description':
+      'Radio Pop Goiaba/UFF is an extension project of Universidade Federal Fluminense linked to NUFEP - Núcleo Fluminense de Estudos e Pesquisas da UFF and coordinated by Professor Roberto Kant de Lima.',
   'turma': 'Class',
   'codigo': 'Code',
   'horario': 'Schedule',
@@ -66,45 +68,46 @@ final Map<String, String> enUS = {
   'plano_estudos': 'Study Plan',
 
   // Transcript
-  'historico' : 'Transcript',
-  'semestre' : 'Semester',
-  'disciplinas' : 'Subjects',
-  'semestre_vazio' : 'Empty semester',
-  'nota' : 'Grade',
+  'historico': 'Transcript',
+  'semestre': 'Semester',
+  'disciplinas': 'Subjects',
+  'semestre_vazio': 'Empty semester',
+  'nota': 'Grade',
 
   // Uniteve Page
-  'historia' : 'History',
-  'contato' : 'Contact',
+  'historia': 'History',
+  'contato': 'Contact',
 
   // Uniteve contact / info
-  'telefone' : 'Phone',
-  'email' : 'Email',
-  'endereco' : 'Address',
+  'telefone': 'Phone',
+  'email': 'Email',
+  'endereco': 'Address',
 
   // Uniteve History
-  'a_uniteve' : 'About Unitevê',
-  'uniteve_historia_descricao' : 'Unitevê was originally created as a cable television channel managed by Universidade Federal Fluminense and began broadcasting in December 2000 from the Institute of Arts and Social Communication (IACS), with support from the IT Superintendency (STI) and the cable operator in Niterói and São Gonçalo. After the suspension of the operator responsible for distribution, its content is currently available on YouTube and social networks. In 2019 Unitevê returned to be part of the Superintendence of Social Communication (SCS) at UFF.',
+  'a_uniteve': 'About Unitevê',
+  'uniteve_historia_descricao':
+      'Unitevê was originally created as a cable television channel managed by Universidade Federal Fluminense and began broadcasting in December 2000 from the Institute of Arts and Social Communication (IACS), with support from the IT Superintendency (STI) and the cable operator in Niterói and São Gonçalo. After the suspension of the operator responsible for distribution, its content is currently available on YouTube and social networks. In 2019 Unitevê returned to be part of the Superintendence of Social Communication (SCS) at UFF.',
 
   // Restaurante Universitário
   'restaurante_universitario': 'University Cafeteria',
-  'menu' : 'Menu',
+  'menu': 'Menu',
 
   // Restaurant menu
-  'detalhes_refeicao' : 'Meal Details',
-  'horario_do_almoço' : 'Lunch Time',
-  'horario_do_jantar' : 'Dinner Time',
-  'ver_horarios' : 'See Schedules',
-  'aberto' : 'Open',
-  'fechado' : 'Closed',
-  'refeicoes' : 'Meals',
-  'ver_refeicoes' : 'See Meals',
-  'restaurantes' : 'Restaurants',
+  'detalhes_refeicao': 'Meal Details',
+  'horario_do_almoço': 'Lunch Time',
+  'horario_do_jantar': 'Dinner Time',
+  'ver_horarios': 'See Schedules',
+  'aberto': 'Open',
+  'fechado': 'Closed',
+  'refeicoes': 'Meals',
+  'ver_refeicoes': 'See Meals',
+  'restaurantes': 'Restaurants',
 
   // Catraca
   'catraca_online': 'Turnstile',
 
   // Internacional
-  'internacional' : 'International'
+  'internacional': 'International',
 
   // Central de atendimento
   'central_de_atendimento': 'Help Center',
