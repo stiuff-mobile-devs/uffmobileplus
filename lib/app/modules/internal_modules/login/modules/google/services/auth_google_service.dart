fb0b92b5c75fcb8fa96a64d569cd9f46f71d443d
JpFontinele-UFF <jfontinele@id.uff.br>
2026-01-28 10:55:16 -0300

    feat: organizacao

--- lib/app/modules/internal_modules/login/modules/google/services/auth_google_service.dart
+++ lib/app/modules/internal_modules/login/modules/google/services/auth_google_service.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:uffmobileplus/app/config/secrets.dart';
 import 'package:uffmobileplus/app/modules/internal_modules/user/data/models/user_google_model.dart';
 import 'package:uffmobileplus/app/modules/internal_modules/user/data/repository/user_google_repository.dart';
 
@@ -10,8 +11,7 @@ class AuthGoogleService {
   final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
 
   late final Future<void> _init = _googleSignIn.initialize(
-    serverClientId:
-        '222693628192-457r7hivgvjgcqtn1aieeiiqkqfqeenr.apps.googleusercontent.com',
+    serverClientId: Secrets.googleServerClientId,
   );
   final UserGoogleRepository _userRepository = UserGoogleRepository();
 
