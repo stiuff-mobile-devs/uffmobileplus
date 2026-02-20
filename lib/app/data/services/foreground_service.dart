60f00c8e4626de78b13d5f59aaa06cfc38f3fed5
João Vítor L. Gonçalves <jvlgoncalves@id.uff.br>
2026-02-20 12:19:23 -0300

    Feat: agora as posições devem oscilar em torno de um raio menor.

--- lib/app/data/services/foreground_service.dart
+++ lib/app/data/services/foreground_service.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:geolocator/geolocator.dart';
@@ -8,7 +6,7 @@ import 'package:uffmobileplus/app/modules/external_modules/monitora_uff/data/pro
 import 'package:uffmobileplus/app/modules/external_modules/monitora_uff/models/user_location_model.dart';
 import 'package:uffmobileplus/firebase_options_tracking.dart';
 
-Timer? _locationTimer;
+//Timer? _locationTimer;
 
 @pragma('vm:entry-point')
 void onStart(ServiceInstance service) async {
@@ -31,25 +29,34 @@ void onStart(ServiceInstance service) async {
   service.invoke('ready');
 }
 
-
 void updateLocation(ServiceInstance service, String iduff, String name) {
-  _locationTimer?.cancel();
-  _locationTimer = Timer.periodic(const Duration(seconds: 5), (timer) async {
-    Position position = await Geolocator.getCurrentPosition(
-      locationSettings: LocationSettings(accuracy: LocationAccuracy.high),
-    );
+  // Configuração do GPS
+  final androidSettings = AndroidSettings(
+    accuracy: LocationAccuracy.high, // TODO: testar outros valores aqui
+    distanceFilter: 0, // Só atualiza se mover mais de 10 metros
+    intervalDuration: Duration(seconds: 5),
+    foregroundNotificationConfig: ForegroundNotificationConfig(
+      notificationTitle: "UFF Mobile Plus",
+      notificationText: "Sua posição está sendo monitorada.",
+      enableWakeLock: true, // TODO: ler a descrição deste atributo.
+    ),
+  );
 
-    if (service is AndroidServiceInstance) {
-      if (await service.isForegroundService()) {
-        service.setForegroundNotificationInfo(
-          title: "ATENÇÃO: monitoramento ativo",
-          content: "Entre no UFF Mobile Plus e desative-o quando necessário!",
-        );
-      }
-    }
+  // TODO: configurar IOSSettings
+
+  // TODO: adicionar condição aqui para saber se serviço é Android ou IOS e seleciona a configuração correta;
+
+  // TODO: trocar androidSettings por locationSettings
+  Geolocator.getPositionStream(locationSettings: androidSettings).listen((
+    Position position,
+  ) async {
+    print("\n\n${position.accuracy}\n\n");
+    // TODO: Filtro de precisão: Se o erro for maior que 20 metros, ignorar
+    // e.g.: if (position.accuracy > 20) return;
 
     String deviceId = (await DeviceService.getBuildNumber()).toString();
 
+    // Atualiza firebase
     if (await FirebaseProvider().doesDocumentExist(deviceId)) {
       await FirebaseProvider().updateLocationAndTimestamp(
         deviceId,
@@ -72,8 +79,54 @@ void updateLocation(ServiceInstance service, String iduff, String name) {
     }
 
     // Envia para o app principal
-    service.invoke('updateLocationLocally', {
-      'position': position
-    });
+    service.invoke('updateLocationLocally', {'position': position});
   });
 }
+
+//void updateLocation(ServiceInstance service, String iduff, String name) {
+//  _locationTimer?.cancel();
+//  _locationTimer = Timer.periodic(const Duration(seconds: 5), (timer) async {
+//    Position position = await Geolocator.getCurrentPosition(
+//      locationSettings: LocationSettings(
+//        accuracy: LocationAccuracy.high,
+//      ),
+//    );
+//
+//    if (service is AndroidServiceInstance) {
+//      if (await service.isForegroundService()) {
+//        service.setForegroundNotificationInfo(
+//          title: "ATENÇÃO: monitoramento ativo",
+//          content: "Entre no UFF Mobile Plus e desative-o quando necessário!",
+//        );
+//      }
+//    }
+//
+//    String deviceId = (await DeviceService.getBuildNumber()).toString();
+//
+//    if (await FirebaseProvider().doesDocumentExist(deviceId)) {
+//      await FirebaseProvider().updateLocationAndTimestamp(
+//        deviceId,
+//        position.latitude,
+//        position.longitude,
+//        DateTime.now(),
+//      );
+//    } else {
+//      await FirebaseProvider().adicionarDados(
+//        UserLocationModel(
+//          id: deviceId,
+//          lat: position.latitude,
+//          lng: position.longitude,
+//          timestamp: DateTime.now(),
+//          nome: name,
+//          iduff: iduff,
+//          isTracked: true,
+//        ),
+//      );
+//    }
+//
+//    // Envia para o app principal
+//    service.invoke('updateLocationLocally', {
+//      'position': position
+//    });
+//  });
+//}
