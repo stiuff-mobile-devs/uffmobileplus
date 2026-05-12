7b852499740e484824560947483609fc05a6c20d
JpFontinele-UFF <jfontinele@id.uff.br>
2026-05-12 14:47:29 -0300

    desabilitando bloqueio de tela temporariamente

--- lib/app/data/services/screen_protector_service.dart
+++ lib/app/data/services/screen_protector_service.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:screen_protector/screen_protector.dart';
+//import 'package:screen_protector/screen_protector.dart';
 
 class ScreenProtectorService {
-   void enableScreenProtection() async {
+  void enableScreenProtection() async {
     // No Android, isso ativa a FLAG_SECURE imediatamente
     // No iOS, prepara o sistema para esconder conteúdo sensível
-    await ScreenProtector.preventScreenshotOn();
-    await ScreenProtector.protectDataLeakageWithBlur();
-      debugPrint("Proteção de tela ATIVADA");
+    //await ScreenProtector.preventScreenshotOn();
+    //await ScreenProtector.protectDataLeakageWithBlur();
+    debugPrint("Proteção de tela ATIVADA");
   }
 
   /// Desativa a proteção de tela
   void disableScreenProtection() async {
-    await ScreenProtector.preventScreenshotOff();
+    //await ScreenProtector.preventScreenshotOff();
     debugPrint("Proteção de tela DESATIVADA");
   }
 }
